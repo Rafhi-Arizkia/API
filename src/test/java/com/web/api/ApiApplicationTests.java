568c4d7f937ab5171713e858d84eb0e293699191
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-18 16:58:29 +0700

    feature:

---
+++ src/test/java/com/web/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
