dcbdaeab3b032438a76af88c90b4f8c6855e86c7
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-19 17:00:03 +0700

    style(Add Annotation):

--- src/main/java/com/web/api/model/repo/CategoryRepo.java
+++ src/main/java/com/web/api/model/repo/CategoryRepo.java
@@ -2,6 +2,8 @@ package com.web.api.model.repo;
 
 import com.web.api.model.entities.CategoryEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<CategoryEntities, Long> {
 }
--- src/main/java/com/web/api/model/repo/ProductRepo.java
+++ src/main/java/com/web/api/model/repo/ProductRepo.java
@@ -2,6 +2,8 @@ package com.web.api.model.repo;
 
 import com.web.api.model.entities.ProductEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends JpaRepository<ProductEntities,Long> {
 }
