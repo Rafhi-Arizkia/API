0ccbc217a10d16c702b7ea1f888a1ad4807789ae
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-19 20:14:13 +0700

    build(Menambahkan code untuk menggabukan data):

--- src/main/java/com/web/api/model/repo/ProductRepo.java
+++ src/main/java/com/web/api/model/repo/ProductRepo.java
@@ -1,9 +1,16 @@
 package com.web.api.model.repo;
 
 import com.web.api.model.entities.ProductEntities;
+import com.web.api.model.entities.SupplierEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface ProductRepo extends JpaRepository<ProductEntities,Long> {
+    @Query("SELECT p.supplierEntities FROM ProductEntities p where p.productId =  :productId")
+    Set<SupplierEntities>findProductEntitiesByProductId(@Param("productId")Long productId);
 }
