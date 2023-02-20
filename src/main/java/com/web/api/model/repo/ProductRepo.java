71cd112a1c5d2d6305efd22abb1c9a330b83df59
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-21 00:06:53 +0700

    fix(Menyeselekain masalah menggabungkan 2 data yaitu data product dan supplier):

--- src/main/java/com/web/api/model/repo/ProductRepo.java
+++ src/main/java/com/web/api/model/repo/ProductRepo.java
@@ -3,6 +3,7 @@ package com.web.api.model.repo;
 import com.web.api.model.entities.ProductEntities;
 import com.web.api.model.entities.SupplierEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
