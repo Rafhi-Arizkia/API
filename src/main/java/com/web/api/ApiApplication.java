98985a5c37eecaaa9f5c643b29beca13274a555d
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-21 01:34:56 +0700

    build: Menambahkan Bean ModelMapper untuk di inject

--- src/main/java/com/web/api/ApiApplication.java
+++ src/main/java/com/web/api/ApiApplication.java
@@ -1,7 +1,9 @@
 package com.web.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +11,8 @@ public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
+    @Bean // untuk menginisialisasi model mapper
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
