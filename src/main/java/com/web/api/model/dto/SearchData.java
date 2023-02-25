7f7eb9098a2d3abf7e48841f3606bea86596ec4d
Rafhi Arizkia <doyokbsk@gmail.com>
2023-02-25 17:49:13 +0700

    build(menambahkan class SearchData): untuk membuat kata kunci pencarian

---
+++ src/main/java/com/web/api/model/dto/SearchData.java
@@ -0,0 +1,14 @@
+package com.web.api.model.dto;
+
+public class SearchData {
+
+    private String searchKey;
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+    }
+}
