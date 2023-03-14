e8cf26d4ddd0d0339cef4711d1acbe264651346c
Rafhi Arizkia <doyokbsk@gmail.com>
2023-03-14 20:51:47 +0700

    build(Menambahkan Field Id):

--- src/main/java/com/web/api/dto/CategoryDTO.java
+++ src/main/java/com/web/api/dto/CategoryDTO.java
@@ -4,8 +4,17 @@ import jakarta.validation.constraints.NotEmpty;
 
 public class CategoryDTO {
 
+    private Long categoryId;
     @NotEmpty(message = "Category name must not be empty")
     private String categoryName;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 
     public String getCategoryName() {
         return categoryName;
