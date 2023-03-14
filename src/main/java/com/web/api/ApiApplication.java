c9ba537aa5c2556e4ca9c6802c1448305067f13e
Rafhi Arizkia <doyokbsk@gmail.com>
2023-03-14 20:47:49 +0700

    build(Menambahkan Bean AuditorAware):

--- src/main/java/com/web/api/ApiApplication.java
+++ src/main/java/com/web/api/ApiApplication.java
@@ -1,11 +1,15 @@
 package com.web.api;
 
+import com.web.api.utils.AuditorAwareImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ApiApplication {
 
     public static void main(String[] args) {
@@ -15,5 +19,10 @@ public class ApiApplication {
     @Bean // untuk menginisialisasi model mapper
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new AuditorAwareImpl();
     }
 }
