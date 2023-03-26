8bf792f57d6a782ebd979161d4365a4e8fa3422f
Rafhi Arizkia <doyokbsk@gmail.com>
2023-03-26 18:35:43 +0700

    build(Menambahkan Anotasi lombok ):

--- src/main/java/com/web/api/model/entities/BaseEntities.java
+++ src/main/java/com/web/api/model/entities/BaseEntities.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -11,7 +13,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
-
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntities<T> {
@@ -26,35 +29,4 @@ public class BaseEntities<T> {
     @LastModifiedDate
     protected Date updateData;
 
-    public T getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(T createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public T getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(T updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateData() {
-        return updateData;
-    }
-
-    public void setUpdateData(Date updateData) {
-        this.updateData = updateData;
-    }
 }
