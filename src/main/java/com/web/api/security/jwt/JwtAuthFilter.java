af9062553e5e8e9dab943314d52fa88136b538d2
Dandelions16 <doyokbsk@gmail.com>
2023-04-09 18:37:01 +0700

    docs(documentation code):

--- src/main/java/com/web/api/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/web/api/security/jwt/JwtAuthFilter.java
@@ -28,35 +28,68 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         this.userDetailsService = userDetailsService;
     }
 
+    /**
+     *
+     * @param request          Objek HttpServletRequest dari permintaan HTTP saat ini
+     * @param response         Objek HttpServletResponse untuk memberikan respons HTTP saat ini
+     * @param filterChain      FilterChain yang dapat digunakan oleh servlet untuk mengalirkan permintaan ke sisa rantai filter
+     * @throws ServletException  Jika terjadi kesalahan pada servlet
+     * @throws IOException   Jika terjadi kesalahan input/output saat melakukan operasi I/O pada servlet
+     * @throws AuthenticationException   Jika terjadi kesalahan saat melakukan autentikasi
+     */
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException, AuthenticationException {
+        // untuk mengambil header authorization
         final String authHeader = request.getHeader("Authorization");
+        // untuk menyimpan token yang sudah dipisahkan dari header authorization
         final String jwt;
+        // untuk menyimpan nama penngguna token jwt menggunakan objek jwt service
         final String userEmail;
 
+        // Jika header Authorization kosong atau tidak diawali dengan "Bearer",
+        // maka kode akan menyelesaikan proses filter dan melanjutkan ke filter selanjutnya
+        // tanpa melakukan proses autentikasi.
+        // Hal ini menandakan bahwa permintaan client tidak mengirimkan token JWT yang valid.
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
         }
 
+        //  kita menggunakan substring(7) karena kita ingin mengambil karakter ke-7 sampai akhir string.
         jwt = authHeader.substring(7);
+
+        // Pada kode ini, kita menggunakan jwtService.extractUsername(jwt) untuk mengambil email user dari token JWT.
+        // Hasil dari method extractUsername kemudian disimpan dalam variabel userEmail yang digunakan pada kode
+        // selanjutnya untuk melakukan validasi token dan autentikasi user.
         userEmail = jwtService.extractUsername(jwt);
 
+        // Memeriksa apakah ada nilai pengguna dan tidak ada objek autententikasi dalam konteks keamanan saat ini
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // selanjutnya memuat UserDetails menggunakan userDetailsService dengan parameter user email
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            // memeriksa apakah token jwt yang diterima dari client(jwt) valid
             if (jwtService.validToken(jwt, userDetails)) {
+                // kode ini untuk menyimpan tentang pengguna yang terautentikasi
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        // userDetails objek yang mempresentasikan pengguna seperti password dan email
                         userDetails,
                         null,
+                        // untuk otoritas yang diberikan kepada pengguna
                         userDetails.getAuthorities()
                 );
+                // digunakan untuk mengisi informasi detail pada objek authenticationToken.
+                // detail tersebut diambil dari informasi request yang dikirim dari pengguna,seperti ip adders dan
+                // browser yang digunakan
                 authenticationToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
+                // bertujuan untuk set objek authenticationToken ke dalam SecurityContextHolder
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
+        // setelah filter dijalankan, maka respon akan dikirimkan kembali ke client.
+        // kode ini bertugas untuk meneruskan permintaan dan respon ke filter selanjutnya dalam filter chain
         filterChain.doFilter(request, response);
     }
 }
